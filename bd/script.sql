be8e57485f084e57c4994e00f28a4bbcfd755a2b
giomafra <giovanna.msilva@sptech.school>
2024-11-19 23:17:26 -0300

    alterando o banco

--- bd/script.sql
+++ bd/script.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE CampHalfBlood;
-
 USE CampHalfBlood;
 
 CREATE TABLE usuario(
@@ -10,7 +9,23 @@ senha VARCHAR(45),
 confirmacao VARCHAR(45),
 personagem INT);
 
-CREATE TABLE quizz(
-idQuiz INT PRIMARY KEY AUTO_INCREMENT,
-tipo VARCHAR(45)
+CREATE TABLE quizz_conhecimento(
+idQuiz_conhec INT PRIMARY KEY AUTO_INCREMENT,
+pontuacao int,
+fkUsuario INT,
+CONSTRAINT fkConhecUsuario FOREIGN KEY (fkUsuario)
+REFERENCES usuario(idUsuario)
 );
+
+CREATE TABLE quizz_personalidade(
+idQuiz_persona INT PRIMARY KEY AUTO_INCREMENT,
+chale int,
+personagem VARCHAR(45),
+fkUsuario INT,
+CONSTRAINT fkPersonUsuario FOREIGN KEY (fkUsuario)
+REFERENCES usuario(idUsuario)
+);
+
+SELECT * FROM quizz_personalidade;
+
+
