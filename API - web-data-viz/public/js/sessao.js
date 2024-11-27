9f98a5c622b1fc574ed6f51779deae95a3663eb1
giomafra <giovanna.msilva@sptech.school>
2024-11-27 14:15:09 -0300

    add btn sair

--- API - web-data-viz/public/js/sessao.js
+++ API - web-data-viz/public/js/sessao.js
@@ -15,7 +15,7 @@ function validarSessao() {
 
 function limparSessao() {
     sessionStorage.clear();
-    window.location = "../login.html";
+    window.location = "../index.html";
 }
 
 // carregamento (loading)
